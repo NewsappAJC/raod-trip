16c7838109c63506d861babfb50fb9394522fae2
John Perry <jgperry@gmail.com>
2013-05-15 17:58:10 -0400

    responsive grid

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,12 @@
-$(function() {
-  var w = 960,
-      h = 1200;
+$(document).ready( function() {
+  var w = $("div#map").width(),
+      h = 600;
 
   var projection = d3.geo.albers()
         .rotate([84.4,0])
         .center([0,33.1])
         .scale([25000])
-        .translate([w/2,h/2]);
+        .translate([w/2,h]);
 
   var path = d3.geo.path().projection(projection);
 
@@ -16,12 +16,31 @@ $(function() {
         .attr("height", h);
 
   d3.json( 'data/expressways.geojson', function(json) {
-    window.json = json;
-    svg.selectAll("path")
-       .data(json.features)
+    var expway = json;
+    window.expway = expway;
+
+    d3.json( 'data/GA400.geojson', function(json) {
+      var ga400 = json;
+      window.ga400 = json;
+      drawMap();
+    });
+  });
+
+  var drawMap = function() {
+    svg.selectAll("expway")
+       .data(expway.features)
        .enter()
        .append("path")
-       .attr("d", path)
-       .style('stroke', 'grey');
-  });
+         .attr("d", path)
+         .style('stroke', 'grey');
+
+    svg.selectAll("ga400")
+      .data(ga400.features)
+      .enter()
+      .append("path")
+        .attr("d", path)
+        .style("stroke", "red")
+        .style("stroke-width", "3px")
+      .on('click', function(){ console.log('click'); });
+  };
 });
