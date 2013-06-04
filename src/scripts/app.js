1876440f5adef10857c371de16f77db955f18bb5
John Perry <jgperry@gmail.com>
2013-06-04 11:54:35 -0400

    some stuff

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,72 @@
 $(document).ready( function() {
-  var north = '#D91818',
-      south = '#F2A71B',
-      east  = '#6C8C26',
-      west  = '#0F808C',
-      currentRoute;
-  var w = $("div#map").width(),
-      h =280;
+  var routeColors = {
+        ga400:   '#D91818',
+        south75: '#F2A71B',
+        east20:  '#6C8C26',
+        west20:  '#0F808C'
+      },
+      routeMaps = [],
+      currentRoute = '',
+      route = "",
+      formatPercent = d3.format(".0%"),
+      selectedPoints,
+      redrawChart;
+
+  // Initial map configuration
+  var mapConfig = {
+    width: $("div#map").width(),
+    height: 280
+  };
 
   var projection = d3.geo.albers()
         .rotate([84.29,0])
         .center([0,33.16])
         .scale([12000])
-        .translate([w/2,h]);
+        .translate([mapConfig.width/2, mapConfig.height]);
 
-  var path = d3.geo.path().projection(projection);
+  var mapPath = d3.geo.path().projection(projection);
 
-  var svg = d3.select("#map")
+  var map = d3.select("#map")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+        .attr("width", mapConfig.width)
+        .attr("height", mapConfig.height);
 
+  // Initial chart configuration
+  var chartConfig = {};
+  chartConfig.margins = {top: 5, right: 10, bottom: 175, left: 50};
+  chartConfig.width   = $("#chart").width() - chartConfig.margins.left - chartConfig.margins.right;
+  chartConfig.height  = 450 - chartConfig.margins.top - chartConfig.margins.bottom;
+
+  chartConfig.scales = {
+    x: d3.scale.ordinal().rangePoints([0, chartConfig.width]),
+    y: d3.scale.linear().range([chartConfig.height, 0]).domain([0,1])
+  };
+
+  chartConfig.axis = {
+    x: d3.svg.axis().scale(chartConfig.scales.x).orient("bottom"),
+    y: d3.svg.axis().scale(chartConfig.scales.y).orient("left").tickFormat(formatPercent)
+  };
+
+  chartConfig.colors = d3.scale.category20c();
+  chartConfig.colors.domain(["below_poverty_pct","100_to_200_pct","200_to_500_pct","gt_500_pct"]);
+  titleText = ["Income below poverty","Income 100% to 200% of poverty","Income 200% to 500% of poverty","Income more than 500% of poverty"];
+
+  var chartArea = d3.svg.area()
+      .x(function(d,i) { return chartConfig.scales.x(i); })
+      .y0(function(d)  { return chartConfig.scales.y(d.y0); })
+      .y1(function(d)  { return chartConfig.scales.y(d.y0 + d.y); })
+      .interpolate("cardinal");
+
+  var stackChartData = d3.layout.stack()
+      .values(function(d) { return d.values; });
+
+  var chart = d3.select("#chart").append("svg")
+      .attr("width", chartConfig.width + chartConfig.margins.left + chartConfig.margins.right)
+      .attr("height", chartConfig.height + chartConfig.margins.top + chartConfig.margins.bottom)
+    .append("g")
+      .attr("transform", "translate(" + chartConfig.margins.left + "," + chartConfig.margins.top + ")");
+
+  // Read data files
   var county, expway, ga400, south75, east20, west20, points;
   d3.json( 'data/county.geojson', function(json) {
     county = json;
@@ -41,8 +88,8 @@ $(document).ready( function() {
 
               d3.csv( 'data/points.csv', function(data) {
                 points = data;
-                window.p = points;
                 drawMap();
+                drawChart("blank");
               });
             });
           });
@@ -51,61 +98,68 @@ $(document).ready( function() {
     });
   });
 
+  //////////////////////////////////////////////////////////////////////////////////
   var drawMap = function() {
-    svg.selectAll("county")
-       .data(county.features)
-       .enter()
-       .append("path")
-         .attr("d", path)
-         .style('stroke', 'white')
-         .style('stroke-width', '3px')
-         .style('fill', '#F2EFF4');
+    // Map layer constructor
+    var addMapLayer = function(container, options) {
+      l = container.selectAll(options.name)
+         .data(options.data)
+         .enter()
+         .append("path")
+          .attr("d", mapPath);
 
-    svg.selectAll("expway")
-       .data(expway.features)
-       .enter()
-       .append("path")
-         .attr("d", path)
-         .style('stroke', 'lightgrey')
-         .style('stroke-width', '2px');
+      if ( typeof options.attr !== "undefined" ) {
+        _.each(options.attr, function(attr) {
+          l.attr(attr.name, attr.value);
+        });
+      }
+      if ( typeof options.attr !== "undefined" ) {
+        _.each(options.style, function(style) {
+          l.style(style.name, style.value);
+        });
+      }
+      return l;
+    };
 
-    svg.selectAll("ga400")
-      .data(ga400.features)
-      .enter()
-      .append("path")
-        .attr("d", path)
-        .attr('class', 'ga400')
-        .style("stroke", north)
-        .style("stroke-width", "0px");
+    // Add county layer to map
+    var countyLayer = addMapLayer(map, {
+      name: "county",
+      data: county.features,
+      attr: [{ name: "class", value: "county"}],
+      style: [
+        { name: "stroke", value: "white" },
+        { name: "stroke-width", value: "3px" },
+        { name: "fill", value: "#F2EFF4" }
+      ]
+    });
 
-    svg.selectAll("south75")
-      .data(south75.features)
-      .enter()
-      .append("path")
-        .attr("d", path)
-        .attr('class', 'south75')
-        .style("stroke", south)
-        .style("stroke-width", "0px");
+    // Add expressway layer to map
+   var expwayLayer = addMapLayer(map, {
+     name: "expway",
+     data: expway.features,
+     attr: [{name:"class", value: "expway"}],
+     style: [
+       { name: "stroke", value: "lightgrey" },
+       { name: "stroke-width", value: "2px" }
+     ]
+   });
 
-    svg.selectAll("east20")
-      .data(east20.features)
-      .enter()
-      .append("path")
-        .attr("d", path)
-        .attr('class', 'east20')
-        .style("stroke", east)
-        .style("stroke-width", "0px");
+   // Add route layers to map
+   _.each(["ga400","south75","east20","west20"], function(route) {
+    var data = eval(route);
+    routeMaps[route] = addMapLayer(map, {
+      name: route,
+      data: data.features,
+      attr: [{name: "class", value: route}],
+      style: [
+        {name: "stroke", value: routeColors[route]},
+        {name: "stroke-width", value: "0px"}
+      ]
+    });
+   });
 
-    svg.selectAll("west20")
-      .data(west20.features)
-      .enter()
-      .append("path")
-        .attr("d", path)
-        .attr('class', 'west20')
-        .style("stroke", west)
-        .style("stroke-width", "0px");
-
-    svg.selectAll("points")
+   // Ad point layer to map
+    map.selectAll("points")
       .data(points)
       .enter()
       .append("circle")
@@ -119,11 +173,146 @@ $(document).ready( function() {
         .style("stroke", "white");
   };
 
+  /////////////////////////////////////////////////////////////////////////////////
+  var drawChart = function(route) {
+    selectedPoints = _.sortBy(_.where(points, { route: route }), function(d) {
+      return d.id;
+    });
+
+    var stackedData = stackChartData(chartConfig.colors.domain().map(function(name, i) {
+      return {
+        name: name,
+        titleText: titleText[i],    // Get tooltip text from titleText array
+        values: selectedPoints.map(function(d, j) {
+          return {x: j, y: d[name] / 100, label: d.to_loc, id: d.id};
+        })
+      };
+    }));
+
+    chartConfig.scales.x.domain(d3.range(selectedPoints.length));
+    chartConfig.axis.x.tickValues(selectedPoints.map(function(d, i) { return d.to_loc; }));
+
+    d3.selectAll(".area").remove();
+    d3.selectAll(".xLabels").remove();
+    d3.selectAll(".x").remove();
+    d3.selectAll(".chart-line").remove();
+    d3.selectAll(".chart-point").remove();
+
+    var tour = chart.selectAll(".browser")
+      .data(stackedData);
+
+    tour.enter().append("g")
+        .attr("class", "browser");
+
+    tour.append("path")
+      .attr("class", "area")
+      .attr("d", function(d) { return chartArea(d.values); })
+      .style("fill", function(d) { return chartConfig.colors(d.name); })
+        .append("title")
+          .text(function(d) { return d.titleText; });
+
+    tourUpdate = d3.transition(tour);
+
+    tourUpdate.transition().select("path").duration(600)
+      .attr("d", function(d) { return chartArea(d.values); });
+
+    var showChartPoint = function(d,i) {
+      var routeData = selectedPoints[i];
+
+      $("#from").text(routeData.from_loc);
+      $("#to").text(routeData.to_loc);
+      $("#gt500").text( routeData.gt_500_pct );
+      $("#gt200").text( routeData["200_to_500_pct"] );
+      $("#gt100").text( routeData["100_to_200_pct"] );
+      $("#lt100").text( routeData.below_poverty_pct );
+      $("#info").removeClass("hidden");
+
+      d3.select(".selected-chart-line").attr("class", "chart-line");
+      d3.select('#route-' + i).attr("class", "selected-chart-line");
+
+      d3.selectAll(".chart-point").attr("r", "0px");
+      d3.selectAll(".point-" + i).attr("r", "4px");
+
+      d3.selectAll(".mapPoint").attr("opacity", 0);
+      d3.select("#mapPoint" + routeData.id ).attr("opacity", 1);
+
+      d3.selectAll(".xLabels").style("fill", "lightgrey");
+      d3.select("#xLabel-" + i).style("fill", "black");
+    };
+
+    chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + chartConfig.height + ")")
+      .call(chartConfig.axis.x)
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("class", "xLabels")
+        .attr("id", function(d,i) {return "xLabel-" + i;})
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", function(d){ return "rotate(-65)"; })
+        .on("mouseover", showChartPoint);
+
+    chart.append("g")
+      .attr("class", "y axis")
+      .call(chartConfig.axis.y);
+
+    var lines = chart.selectAll("chart-line")
+      .data(selectedPoints);
+
+    if ( route !== "blank" ) {
+      lines.enter().append("svg:line")
+          .attr("id", function(d,i) { return 'route-' + i; })
+          .attr("class", "chart-line")
+          .attr("x1", function(d,i) { return chartConfig.scales.x(i); })
+          .attr("x2", function(d,i) { return chartConfig.scales.x(i); })
+          .attr("y1", 0)
+          .attr("y2", chartConfig.height);
+
+      d3.values(stackedData).forEach(function(point) {
+        chart.selectAll("chart-point")
+          .data(point.values)
+          .enter().append("svg:circle")
+            .attr("cx", function(d) { return chartConfig.scales.x(d.x); })
+            .attr("cy", function(d) { return chartConfig.scales.y(d.y0 + d.y); })
+            .attr("r", "0px")
+            .attr("id", function(d) { return point.name + "-" + d.x; })
+            .attr("class", function(d) { return "chart-point point-" + d.x; })
+          .append("text")
+            .text(function(d) { return selectedPoints[d.x][point.name] + "%"; })
+            .attr("x", function(d) { return chartConfig.scales.x(d.x); })
+            .attr("y", function(d) { return chartConfig.scales.y(d.y0 + d.y); });
+      });
+      showChartPoint([],0);
+    }
+  };  // drawChart
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // Show routes on the map in response to mouse events
+  var showRoute = function(route) {
+    if (currentRoute !== '') {
+      d3.selectAll('.' + currentRoute)
+        .transition().duration(500)
+        .style('stroke-width', '0');
+    }
+
+    d3.selectAll('.' + route)
+      .transition().delay(500).duration(500)
+      .style('stroke-width', '3px')
+      .style('opacity', 1);
+    currentRoute = route;
+  };
+
+  // Mouse events on route buttons
   d3.selectAll('.route')
     .on('click', function() {
-      console.log(this.id);
-      showRoute(this.id);
       d3.event.preventDefault();
+      showRoute(this.id);
+      if ( typeof selectedPoints === "undefined" ) {
+        drawChart(this.id);
+      } else {
+        drawChart(this.id);
+      }
     })
     .on('mouseover', function() {
       if ( this.id === currentRoute ) {
@@ -133,7 +322,7 @@ $(document).ready( function() {
       }
       d3.selectAll('.' + this.id)
         .style('stroke-width', '6px')
-        .style('opacity', 0.35);
+        .style('opacity', 0.25);
     })
     .on('mouseout', function() {
       if ( this.id === currentRoute ) {
@@ -145,15 +334,4 @@ $(document).ready( function() {
         .style('stroke-width', '0')
         .style('opacity', 1);
     });
-
-    var showRoute = function(route) {
-      d3.selectAll('.' + currentRoute)
-        .transition().duration(750)
-        .style('stroke-width', '0');
-      d3.selectAll('.' + route)
-        .transition().duration(1500)
-        .style('stroke-width', '3px')
-        .style('opacity', 1);
-      currentRoute = route;
-    };
 });
--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,86 +1,77 @@
-$(function(route){
-  route = route || 'ga400';
 
-  var margin = {top: 5, right: 20, bottom: 175, left: 40},
-      width = $("#chart").width() - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+$(function(){
+  var route = "ga400",
+      formatPercent = d3.format(".0%"),
+      chartConfig = {};
 
-  var formatPercent = d3.format(".0%");
+  chartConfig.margins = {top: 5, right: 20, bottom: 175, left: 50};
+  chartConfig.width   = $("#chart").width() - chartConfig.margins.left - chartConfig.margins.right;
+  chartConfig.height  = 500 - chartConfig.margins.top - chartConfig.margins.bottom;
 
-  var xScale = d3.scale.ordinal()
-        .rangePoints([0, width]),
-      yScale = d3.scale.linear()
-        .range([height, 0]);
+  chartConfig.scales = {
+    x: d3.scale.ordinal().rangePoints([0, chartConfig.width]),
+    y: d3.scale.linear().range([chartConfig.height, 0])
+  };
 
-  var color = d3.scale.category20c();
+  chartConfig.axis = {
+    x: d3.svg.axis().scale(chartConfig.scales.x).orient("bottom"),
+    y: d3.svg.axis().scale(chartConfig.scales.y).orient("left").tickFormat(formatPercent)
+  };
 
-  var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient("bottom");
+  chartConfig.colors = d3.scale.category20c();
 
-  var yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient("left")
-      .tickFormat(formatPercent);
 
-  var area = d3.svg.area()
-      .x(function(d, i) { return xScale(i); })
-      .y0(function(d) { return yScale(d.y0); })
-      .y1(function(d) { return yScale(d.y0 + d.y); })
+  var chartArea = d3.svg.area()
+      .x(function(d, i) { return chartConfig.scales.x(i); })
+      .y0(function(d)   { return chartConfig.scales.y(d.y0); })
+      .y1(function(d)   { return chartConfig.scales.y(d.y0 + d.y); })
       .interpolate("cardinal");
 
-  var stack = d3.layout.stack()
+  var chartStack = d3.layout.stack()
       .values(function(d) { return d.values; });
 
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+  var chart = d3.select("#chart").append("svg")
+      .attr("width", chartConfig.width + chartConfig.margins.left + chartConfig.margins.right)
+      .attr("height", chartConfig.height + chartConfig.margins.top + chartConfig.margins.bottom)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  window.svg = svg;
+      .attr("transform", "translate(" + chartConfig.margins.left + "," + chartConfig.margins.top + ")");
 
+  var selectedPoints;
+  ////////////////////////////////////////////////////////////////////////////////////
   d3.csv("data/points.csv", function(error, data) {
-    data = _.sortBy(data, function(d){ return d.id; });
-    window.d = data;
-    color.domain(["below_poverty_pct","100_to_200_pct","200_to_500_pct","gt_500_pct"]);
-    window.c = color;
+    selectedPoints = _.sortBy(_.where(data, { route: route }),
+                      function(d){ return d.id; });
 
-    var browsers = stack(color.domain().map(function(name) {
+    chartConfig.colors.domain(["below_poverty_pct","100_to_200_pct","200_to_500_pct","gt_500_pct"]);
+
+    var browsers = chartStack(chartConfig.colors.domain().map(function(name) {
       return {
         name: name,
-        values: data.map(function(d, i) {
+        values: selectedPoints.map(function(d, i) {
           return {x: i, y: d[name] / 100};
         })
       };
     }));
-    window.b = browsers;
 
-    xScale.domain(d3.range(data.length));
-    xAxis.tickValues(data.map(function(d) { return d.to_loc; }));
+    chartConfig.scales.x.domain(d3.range(selectedPoints.length));
+    chartConfig.axis.x.tickValues(selectedPoints.map(function(d) { return d.to_loc; }));
 
-    var browser = svg.selectAll(".browser")
+    var browser = chart.selectAll(".browser")
       .data(browsers)
       .enter().append("g")
         .attr("class", "browser");
 
     browser.append("path")
       .attr("class", "area")
-      .attr("d", function(d) { return area(d.values); })
-      .style("fill", function(d) { return color(d.name); })
+      .attr("d", function(d) { return chartArea(d.values); })
+      .style("fill", function(d) { return chartConfig.colors(d.name); })
       .append("title")
         .text(function(d) { return d.name; });
 
-    // browser.append("text")
-    //   .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-    //   .attr("transform", function(d) { return "translate(" + xScale(d.value.location) + "," + yScale(d.value.y0 + d.value.y / 2) + ")"; })
-    //   .attr("x", -6)
-    //   .attr("dy", ".35em")
-    //   .text(function(d) { return d.name; });
-
-    svg.append("g")
+    chart.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + chartConfig.height + ")")
+      .call(chartConfig.axis.x)
       .selectAll("text")
         .style("text-anchor", "end")
         .attr("class", "xLabels")
@@ -88,7 +79,7 @@ $(function(route){
         .attr("dy", ".15em")
         .attr("transform", function(d){ return "rotate(-65)"; })
         .on("mouseover", function(d,i) {
-          routeData = data[i];
+          routeData = selectedPoints[i];
 
           $("#from").text(routeData.from_loc);
           $("#to").text(routeData.to_loc);
@@ -105,42 +96,41 @@ $(function(route){
           d3.selectAll(".point-" + i).attr("r", "4px");
 
           d3.selectAll(".mapPoint").attr("opacity", 0);
-          console.log("#mapPoint" + routeData.id);
           d3.select("#mapPoint" + routeData.id ).attr("opacity", 1);
 
-          d3.selectAll(".xLabels").style("fill", "lightgrey")
+          d3.selectAll(".xLabels").style("fill", "lightgrey");
           d3.select(this).style("fill", "black");
         });
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+    chart.append("g")
+      .attr("class", "y axis")
+      .call(chartConfig.axis.y);
 
-    svg.selectAll("route-line")
-      .data(data)
+    chart.selectAll("route-line")
+      .data(selectedPoints)
       .enter().append("svg:line")
-        .attr("id", function(d,i) { return 'route-' + i })
+        .attr("id", function(d,i) { return 'route-' + i; })
         .attr("class", "route-line")
-        .attr("x1", function(d,i) { return xScale(i); })
-        .attr("x2", function(d,i) { return xScale(i); })
+        .attr("x1", function(d,i) { return chartConfig.scales.x(i); })
+        .attr("x2", function(d,i) { return chartConfig.scales.x(i); })
         .attr("y1", 0)
-        .attr("y2", height);
+        .attr("y2", chartConfig.height);
 
     d3.values(browsers).forEach(function(brows) {
-      svg.selectAll("seg-point")
+      chart.selectAll("seg-point")
         .data(brows.values)
         .enter().append("svg:circle")
-          .attr("cx", function(d) { return xScale(d.x); })
-          .attr("cy", function(d) { return yScale(d.y0 + d.y); })
+          .attr("cx", function(d) { return chartConfig.scales.x(d.x); })
+          .attr("cy", function(d) { return chartConfig.scales.y(d.y0 + d.y); })
           .attr("r", "0px")
-          .attr("id", function(d) { return brows.name + "-" + d.x })
+          .attr("id", function(d) { return brows.name + "-" + d.x; })
           .attr("class", function(d) { return "chartPoint point-" + d.x; })
           .style("fill", "black")
           .style("stroke", "white")
         .append("text")
-          .text(function(d) { return data[d.x][brows.name] + "%"; })
-          .attr("x", function(d) { return xScale(d.x); })
-          .attr("y", function(d) { return yScale(d.y0 + d.y); });
+          .text(function(d) { return selectedPoints[d.x][brows.name] + "%"; })
+          .attr("x", function(d) { return chartConfig.scales.x(d.x); })
+          .attr("y", function(d) { return chartConfig.scales.y(d.y0 + d.y); });
     });
   });
 });
